5095fb4feb6ce251e19e6d4fd07f03c7612dafb5
sagar0840 <sagarkale55556@gmail.com>
2023-04-13 20:47:21 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        //1.Find maximum number from given 3 numbers.
+
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            // set the value of the three numbers
+            num1 = 10;
+            num2 = 20;
+            num3 = 50;
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest");
+
+                }
+                else
+                {
+                    Console.Write("Number three is the largest");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the largest");
+            else
+                Console.Write("Number three is the largest");
+        }
+    }
+}
